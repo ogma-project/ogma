0373cfd90ff5f614b8f14e477ae4d5a34cd49c59
Thomas Letan <contact@thomasletan.fr>
2017-08-21 09:10:36 +0200

    Rely on Selectable typeclass rather than Filters data

--- src/Web/Ogma/Resource.hs
+++ src/Web/Ogma/Resource.hs
@@ -16,23 +16,46 @@ import Data.Typeable (Typeable)
 import Data.Proxy    (Proxy(..))
 import Control.Monad.Identity
 
+class Selectable e where
+  data Selector e :: *
+
+  select     :: Selector e -> e -> Bool
+  nothing    :: Proxy e -> Selector e
+  everything :: Proxy e -> Selector e
+
 class Graph spec where
   data Edges spec
-  data Filters spec
 
-data a :<>: b
+instance (Selectable a, Selectable b) => Selectable (Either a b) where
+  data Selector (Either a b) = Prod (Selector a) (Selector b)
+
+  select (Prod f _) (Left x) = select f x
+  select (Prod _ f') (Right y) = select f' y
+
+  nothing _ = Prod (nothing Proxy) (nothing Proxy)
+  everything _ = Prod (everything Proxy) (everything Proxy)
+
+data (a :: *) :<>: (b :: *)
   deriving (Typeable)
 
 instance (Graph s, Graph s') => Graph (s :<>: s') where
   data Edges (s :<>: s') = ELeft (Edges s)
                          | ERight (Edges s')
-  data Filters (s :<>: s') = Prod (Filters s) (Filters s')
 
-class (Graph s, Monad m) => GraphMonad s m | s -> m where
-  selectEdges :: Proxy s -> Filters s -> m [Edges s]
+instance (Graph s, Graph s', Selectable (Edges s), Selectable (Edges s')) => Selectable (Edges (s :<>: s')) where
+  data Selector (Edges (s :<>: s')) = EProd (Selector (Edges s)) (Selector (Edges s'))
+
+  select (EProd f _) (ELeft x) = select f x
+  select (EProd _ f') (ERight y) = select f' y
+
+  nothing _ = EProd (nothing Proxy) (nothing Proxy)
+  everything _ = EProd (everything Proxy) (everything Proxy)
+
+class (Graph s, Monad m, Selectable (Edges s)) => GraphMonad s m | s -> m where
+  selectEdges :: Proxy s -> Selector (Edges s) -> m [Edges s]
 
 instance (GraphMonad s m, GraphMonad s' m) => GraphMonad (s :<>: s') m where
-  selectEdges _ (Prod f f') = do
+  selectEdges _ (EProd f f') = do
     res  <- selectEdges Proxy f
     res' <- selectEdges Proxy f'
     pure $ (ELeft <$> res) ++ (ERight <$> res')
@@ -48,11 +71,13 @@ data prem :-> to
   deriving (Typeable)
 infix 8 :->
 
-class Selectable e where
-  data Selector e :: *
-
-  select :: Selector e -> e -> Bool
-
 instance (Fetchable a, Selectable e, Fetchable b) => Graph (a -: e :-> b) where
-  data Edges (a -: e :-> b) = Edge (Id a) (Id b)
-  data Filters (a -: e :-> b) = F (Selector e)
+  data Edges (a -: e :-> b) = Edge (Id a) e (Id b)
+
+-- TODO: Selection should be possible for the nodes to
+instance (Graph (a -: e :-> b), Selectable e) => Selectable (Edges (a -: e :-> b)) where
+  data Selector (Edges (a -: e :-> b)) = ES (Selector e)
+
+  select (ES sel) (Edge _ l _) = select sel l
+  nothing _ = ES $ nothing Proxy
+  everything _ = ES $ everything Proxy
