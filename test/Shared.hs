1a2716e572cd309a55a1d400eb2f2de8c0285aa5
lthms <contact@thomasletan.fr>
2017-08-22 23:33:15 +0200

    Improve the code coverage of the existing codebase (#3)

---
+++ test/Shared.hs
@@ -0,0 +1,25 @@
+module Shared where
+
+import           Data.Aeson
+import           Data.Proxy
+import           Test.Hspec      (Spec, describe, it, shouldBe)
+import           Test.QuickCheck (Arbitrary (arbitrary), Property, oneof,
+                                  property)
+
+(-->) :: Bool -> Bool -> Bool
+x --> y = not x || y
+
+reflexivity :: (Arbitrary a, Eq a, Show a) => (a -> a -> Bool) -> Property
+reflexivity f = property $ \x -> f x x
+
+symmetry :: (Arbitrary a, Eq a, Show a) => (a -> a -> Bool) -> Property
+symmetry f = property $ \x y -> f x y --> f y x
+
+transitivy :: (Arbitrary a, Eq a, Show a) => (a -> a -> Bool) -> Property
+transitivy f =  property $ \x y z -> f x y --> f y z --> f x z
+
+checkRelation :: (Arbitrary a, Eq a, Show a) => String -> (a -> a -> Bool) -> Spec
+checkRelation name f = describe (name ++ " is a relation") $ do
+  it "should be reflexive" $ reflexivity f
+  it "should be symmetric" $ symmetry f
+  it "should be transitive" $ reflexivity f
